1bd3584bb6f4c632face251e43b6b63d1580f208
Sahil Kumar <173098946+Sahil5273@users.noreply.github.com>
2026-03-14 20:44:06 +0530

    Clear Firebase config keys for security
    Removed sensitive Firebase configuration keys.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Replace these with the keys Firebase gives you!
 const firebaseConfig = {
-  apiKey: "AIzaSyCN9009Qdx9yeJussgRYEiUIhuA_zdJgns",
-  authDomain: "sharebyte-ff1a8.firebaseapp.com",
-  projectId: "sharebyte-ff1a8",
-  storageBucket: "sharebyte-ff1a8.firebasestorage.app",
-  messagingSenderId: "545197199561",
-  appId: "1:545197199561:web:feb8bee40c064e99873d18",
-  measurementId: "G-881VJXNLCB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
